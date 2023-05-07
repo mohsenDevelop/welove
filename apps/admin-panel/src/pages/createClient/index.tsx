0c2da396ccfb9cd2f7c0698199146ffbfe8fd739
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-05-07 10:10:53 +0330

    add bread crumb

--- apps/admin-panel/src/pages/createClient/index.tsx
+++ apps/admin-panel/src/pages/createClient/index.tsx
@@ -1,6 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+
+import { BreadCrumb, Paper } from 'ui';
+import { CLIENT_MANAGEMENT_URL } from '@config/urls';
+
 const CreateClientPage = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>CreateClientPage</div>
+        <Stack>
+            <BreadCrumb
+                list={[
+                    { link: CLIENT_MANAGEMENT_URL, name: 'Clients' },
+                    { link: '', name: 'Add new client' },
+                ]}
+                onClick={(link: string) => navigate(link)}
+            />
+            <Paper
+                sx={{ mt: '24px', p: '30px 24px' }}
+            >
+
+            </Paper>
+        </Stack>
     );
 };
 
