276a30a076929d25dce82f7eced6bb99ab7d90a0
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-05-30 14:30:25 +0330

    add back button for copyitem

--- apps/client-panel/src/pages/news/components/copyItem/index.tsx
+++ apps/client-panel/src/pages/news/components/copyItem/index.tsx
@@ -2,8 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
-import { BreadCrumb, Paper } from 'ui';
-import { NEWS_URL } from '@config/urls';
+import { Paper, BackButton } from 'ui';
 import CopyItemForm from './CopyItemForm';
 
 const CopyItemPage = () => {
@@ -12,12 +11,8 @@ const CopyItemPage = () => {
 
     return (
         <Stack>
-            <BreadCrumb
-                list={[
-                    { link: NEWS_URL, name: 'News' },
-                    { link: '', name: 'Add News item' },
-                ]}
-                onClick={(link: string) => navigate(link)}
+            <BackButton
+                onClick={() => navigate(-1)}
             />
 
             <Paper
