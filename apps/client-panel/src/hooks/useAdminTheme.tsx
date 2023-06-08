ce108ffbd2cbbb14b7290a5b52ec296de9f2f4a9
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-06-08 10:29:53 +0330

    fix client build script

--- apps/client-panel/src/hooks/useAdminTheme.tsx
+++ apps/client-panel/src/hooks/useAdminTheme.tsx
@@ -1,9 +1,10 @@
 import { useContext } from 'react';
-import { AdminThemeContext } from '@pages/createClient/components/adminTheme/AdminThemeProvider';
+// import { AdminThemeContext } from '@pages/createClient/components/adminTheme/AdminThemeProvider';
 
 
 export const UseAdminTheme = () => {
-    const context = useContext(AdminThemeContext);
+    // const context = useContext(AdminThemeContext);
+    const context = undefined;
     if (context === undefined) {
         throw new Error("UseAdminTheme must be used within a AdminThemeProvider");
     }
