1c369909e1f856ee92a11821b2667de7ec38f8be
mohsen kermanifar <mohsen.kermanifar@gmail.com>
2023-06-08 11:42:50 +0330

    complete setting, notification tab

--- apps/client-panel/src/pages/setting/components/pushNotifs/NotifList.tsx
+++ apps/client-panel/src/pages/setting/components/pushNotifs/NotifList.tsx
@@ -1,26 +1,79 @@
 import { useState } from 'react';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 import { colorPalette, Switch } from 'ui';
 
 const NotifList = () => {
 
-    const [state, setState] = useState({
+    const [notifState, setNotifState] = useState<any>({
         gilad: true,
         jason: true,
         antoine: true,
+        referralMobile: true,
     });
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setState({
-            ...state,
+    const [emailState, setEmailState] = useState<any>({
+        shareEmail: true,
+        applicationEmail: true,
+        hireEmail: true,
+        referralEmail: true,
+    });
+
+    const handleChangeNotif = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNotifState({
+            ...notifState,
+            [event.target.name]: event.target.checked,
+        });
+    };
+    const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmailState({
+            ...emailState,
             [event.target.name]: event.target.checked,
         });
     };
 
-    const handleAllSwitch = () => {
-        Object.values(state).every((_key) => _key === false) ? setState({ gilad: true, jason: true, antoine: true }) : setState({ gilad: false, jason: false, antoine: false });
+    const handleAllPushNotifSwitch = () => {
+        Object.values(notifState).every((_key) => _key === false) ?
+            setNotifState(
+                {
+                    gilad: true,
+                    jason: true,
+                    antoine: true,
+                    referralMobile: true,
+                }
+            )
+            :
+            setNotifState(
+                {
+                    gilad: false,
+                    jason: false,
+                    antoine: false,
+                    referralMobile: false,
+                }
+            );
+    };
+
+    const handleAllEmailSwitch = () => {
+        Object.values(emailState).every((_key) => _key === false) ?
+            setEmailState(
+                {
+                    shareEmail: true,
+                    applicationEmail: true,
+                    hireEmail: true,
+                    referralEmail: true,
+                }
+            )
+            :
+            setEmailState(
+                {
+                    shareEmail: false,
+                    applicationEmail: false,
+                    hireEmail: false,
+                    referralEmail: false,
+                }
+            );
     };
 
     return (
@@ -29,8 +82,9 @@ const NotifList = () => {
         >
             <Stack
                 flexDirection={'row'}
-                alignItems={'center'}
+                alignItems={'flex-start'}
                 justifyContent={'space-between'}
+                pr={'50px'}
             >
                 <Typography
                     variant={'h1'}
@@ -40,15 +94,63 @@ const NotifList = () => {
                     Show notifications for
                 </Typography>
 
-                <Typography
-                    fontSize={12}
-                    fontWeight={500}
-                    color={colorPalette.pink200}
-                    sx={{ cursor: 'pointer', userSelect: 'none' }}
-                    onClick={handleAllSwitch}
+                <Stack
+                    flexDirection={'row'}
+                    gap={'65px'}
                 >
-                    {Object.values(state).every((_key) => _key === false) ? 'Turn on all notifications' : 'Turn off all notifications'}
-                </Typography>
+
+                    <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        gap={'34px'}
+                    >
+                        <Typography
+                            fontSize={14}
+                            fontWeight={600}
+                        >
+                            Email
+                        </Typography>
+                        <Typography
+                            fontSize={12}
+                            fontWeight={500}
+                            color={colorPalette.pink200}
+                            sx={{ cursor: 'pointer', userSelect: 'none' }}
+                            onClick={handleAllEmailSwitch}
+                        >
+                            {Object.values(emailState).every((_key) => _key === false) ? 'Turn on all email' : 'Turn off all email'}
+                        </Typography>
+                    </Box>
+
+                    <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        gap={'7px'}
+                    >
+                        <Typography
+                            fontSize={14}
+                            fontWeight={600}
+                        >
+                            Mobile
+                        </Typography>
+                        <Typography
+                            fontSize={12}
+                            fontWeight={400}
+                            color={colorPalette.gray550}
+                        >
+                            Push notifications
+                        </Typography>
+                        <Typography
+                            fontSize={12}
+                            fontWeight={500}
+                            color={colorPalette.pink200}
+                            sx={{ cursor: 'pointer', userSelect: 'none' }}
+                            onClick={handleAllPushNotifSwitch}
+                        >
+                            {Object.values(notifState).every((_key) => _key === false) ? 'Turn on all mobile' : 'Turn off all mobile'}
+                        </Typography>
+                    </Box>
+
+                </Stack>
 
             </Stack>
 
@@ -68,14 +170,27 @@ const NotifList = () => {
                         fontSize={14}
                         fontWeight={400}
                     >
-                        Lorem ipsum
+                        Share
                     </Typography>
 
-                    <Switch
-                        checked={state.gilad}
-                        onChange={handleChange}
-                        name='gilad'
-                    />
+                    <Box
+                        display={'flex'}
+                        alignItems={'center'}
+                        gap={'129px'}
+                    >
+
+                        <Switch
+                            checked={emailState.shareEmail}
+                            onChange={handleChangeEmail}
+                            name='shareEmail'
+                        />
+                        <Switch
+                            checked={notifState.gilad}
+                            onChange={handleChangeNotif}
+                            name='gilad'
+                        />
+
+                    </Box>
                 </Stack>
 
                 <Stack
@@ -83,20 +198,33 @@ const NotifList = () => {
                     alignItems={'center'}
                     justifyContent={'space-between'}
                     p={'34px 95px 34px 24px'}
-                    border={`1px solid ${colorPalette.white}`}
                 >
                     <Typography
                         fontSize={14}
                         fontWeight={400}
                     >
-                        Lorem ipsum
+                        Application
                     </Typography>
 
-                    <Switch
-                        checked={state.jason}
-                        onChange={handleChange}
-                        name='jason'
-                    />
+                    <Box
+                        display={'flex'}
+                        alignItems={'center'}
+                        gap={'129px'}
+                    >
+                        <Switch
+                            checked={emailState.applicationEmail}
+                            onChange={handleChangeEmail}
+                            name='applicationEmail'
+                        />
+
+                        <Switch
+                            checked={notifState.jason}
+                            onChange={handleChangeNotif}
+                            name='jason'
+                        />
+
+                    </Box>
+
                 </Stack>
 
                 <Stack
@@ -111,14 +239,61 @@ const NotifList = () => {
                         fontSize={14}
                         fontWeight={400}
                     >
-                        Lorem ipsum
+                        Hire
                     </Typography>
 
-                    <Switch
-                        checked={state.antoine}
-                        onChange={handleChange}
-                        name='antoine'
-                    />
+                    <Box
+                        display={'flex'}
+                        alignItems={'center'}
+                        gap={'129px'}
+                    >
+                        <Switch
+                            checked={emailState.hireEmail}
+                            onChange={handleChangeEmail}
+                            name='hireEmail'
+                        />
+                        <Switch
+                            checked={notifState.antoine}
+                            onChange={handleChangeNotif}
+                            name='antoine'
+                        />
+
+                    </Box>
+
+                </Stack>
+
+                <Stack
+                    flexDirection={'row'}
+                    alignItems={'center'}
+                    justifyContent={'space-between'}
+                    p={'34px 95px 34px 24px'}
+                >
+                    <Typography
+                        fontSize={14}
+                        fontWeight={400}
+                    >
+                        Only show me a notification when I get a new referral
+                    </Typography>
+
+                    <Box
+                        display={'flex'}
+                        alignItems={'center'}
+                        gap={'129px'}
+                    >
+                        <Switch
+                            checked={emailState.referralEmail}
+                            onChange={handleChangeEmail}
+                            name='referralEmail'
+                        />
+
+                        <Switch
+                            checked={notifState.referralMobile}
+                            onChange={handleChangeNotif}
+                            name='referralMobile'
+                        />
+
+                    </Box>
+
                 </Stack>
 
             </Stack>
